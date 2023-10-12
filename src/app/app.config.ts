c06b1a6b970a6eb6bc53178edff13bdc56061da9
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-10-12 12:11:32 +0200

    Add Angular fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from 'src/environments/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => getAuth()),
+      provideFirestore(() => getFirestore())
+    ),
+  ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'ngrx-income',
+    appId: '1:826742156380:web:5449bd6ac521a3344c0a99',
+    storageBucket: 'ngrx-income.appspot.com',
+    apiKey: 'AIzaSyDLIcuA2_L-OafwW_EH0JrujnFZ3zRj30c',
+    authDomain: 'ngrx-income.firebaseapp.com',
+    messagingSenderId: '826742156380',
+    measurementId: 'G-21NFMBNTX3',
+  },
+};
