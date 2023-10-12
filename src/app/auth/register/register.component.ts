0da6a6d266fd3c7045b4f61b53e9964712b864a8
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-10-12 11:34:54 +0200

    Create register form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'ni-register',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export default class RegisterComponent {
+export default class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+  fb = inject(FormBuilder);
 
+  ngOnInit(): void {
+    this.registerForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  crearUsuario() {
+    console.log(this.registerForm)
+    console.log(this.registerForm.valid)
+    console.log(this.registerForm.value)
+  }
 }
