1be82b88d356639ca32ad183130b21638b402106
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-10-14 13:20:33 +0200

    Install NgRx

---
+++ src/app/shared/ui.reducer.ts
@@ -0,0 +1,21 @@
+import { Action, createReducer, on } from '@ngrx/store';
+import { isLoading, stopLoading } from './ui.actions';
+
+export interface State {
+    isLoading: boolean;
+}
+
+export const initialState: State = {
+    isLoading: false,
+}
+
+const _counterReducer = createReducer(initialState,
+
+    on(isLoading, state => ({ ...state, isLoading: true })),
+    on(stopLoading, state => ({ ...state, isLoading: false })),
+
+);
+
+export function counterReducer(state: State | undefined, action: Action) {
+    return _counterReducer(state, action);
+}
