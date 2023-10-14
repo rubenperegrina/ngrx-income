412806a9bb03165ea9a44ee6106aaf3775cad7a8
Rubén Peregrina Aroca <ruben.peregrina@in2.es>
2023-10-14 16:35:28 +0200

    Add loading buttons for login and register

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,47 +1,58 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2';
-
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as ui from '../../shared/ui.actions';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'ni-login',
   standalone: true,
-  imports: [CommonModule, RouterLink, ReactiveFormsModule],
+  imports: [NgIf, RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export default class LoginComponent {
+export default class LoginComponent implements OnInit, OnDestroy {
   loginForm!: FormGroup;
+  isLoading: boolean = false;
+  uiSubscription!: Subscription;
+
   fb = inject(FormBuilder);
   autService = inject(AuthService);
   router = inject(Router);
+  store: Store<AppState> = inject(Store<AppState>);
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
-      correo: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      correo: ['ruben1@gmail.com', [Validators.required, Validators.email]],
+      password: ['123456', [Validators.required, Validators.minLength(6)]]
     });
+
+    this.uiSubscription = this.store.select('ui').subscribe(ui => {
+      console.log('cargando subs')
+      this.isLoading = ui.isLoading
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.uiSubscription.unsubscribe();
   }
 
   loginUsuario() {
     if (this.loginForm.invalid) return;
-
-    Swal.fire({
-      title: 'Espere por favor...',
-      didOpen: () => {
-        Swal.showLoading();
-      }
-    });
+    this.store.dispatch(ui.isLoading());
 
     const { correo, password } = this.loginForm.value;
     this.autService.loginUsuario(correo, password).then(
       credentials => {
-        Swal.close();
+        this.store.dispatch(ui.stopLoading());
         this.router.navigate(['/']);
       })
       .catch(err => {
+        this.store.dispatch(ui.stopLoading());
         Swal.fire({
           title: 'Oops...',
           text: err.message,
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,22 +1,30 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject, OnDestroy } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import Swal from 'sweetalert2';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as ui from '../../shared/ui.actions';
 
 @Component({
   selector: 'ni-register',
   standalone: true,
-  imports: [CommonModule, RouterLink, ReactiveFormsModule],
+  imports: [NgIf, RouterLink, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export default class RegisterComponent implements OnInit {
+export default class RegisterComponent implements OnInit, OnDestroy {
   registerForm!: FormGroup;
+  isLoading: boolean = false;
+  uiSubscription!: Subscription;
+
   fb = inject(FormBuilder);
   autService = inject(AuthService);
   router = inject(Router);
+  store: Store<AppState> = inject(Store<AppState>);
 
   ngOnInit(): void {
     this.registerForm = this.fb.group({
@@ -24,25 +32,29 @@ export default class RegisterComponent implements OnInit {
       correo: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(6)]]
     });
+
+    this.uiSubscription = this.store.select('ui').subscribe(ui => {
+      console.log('cargando subs')
+      this.isLoading = ui.isLoading
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.uiSubscription.unsubscribe();
   }
 
   crearUsuario() {
     if (this.registerForm.invalid) return;
-
-    Swal.fire({
-      title: 'Espere por favor...',
-      didOpen: () => {
-        Swal.showLoading();
-      }
-    });
+    this.store.dispatch(ui.isLoading());
 
     const { nombre, correo, password } = this.registerForm.value;
     this.autService.crearUsuario(nombre, correo, password).then(
       credentials => {
-        Swal.close();
+        this.store.dispatch(ui.stopLoading());
         this.router.navigate(['/']);
       })
       .catch(err => {
+        this.store.dispatch(ui.stopLoading());
         Swal.fire({
           title: 'Oops...',
           text: err.message,
